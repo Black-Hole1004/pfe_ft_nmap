123e0e27b01d3965ca05259c3206817a3e0c0b16
Chafik-Aymane <helloaymane@gmail.com>
2026-05-12 15:35:46 +0100

    parser testing, mzl kant3lmo

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include "functions.hpp"
+#include "Parser.hpp"
 
 t_scan g_scan = {};
 
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    (void)argv;
+    Parser parser;
+    parser.parseArgs(argc, argv);
+
+    if(parser.target.empty()){
+        std::cout << "empty" << std::endl;
+    } else {
+        std::cout << "target = " << parser.target << std::endl;
+    }
     return 0;
 }
--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -1,18 +1,32 @@
 #include "Parser.hpp"
+#include "functions.hpp"
 
-Parser::Parser()
+// constructor + init target to empty string
+Parser::Parser() : target("") 
 {
 }
 
+// testing parsing, more flags later!
 void Parser::parseArgs(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
+    for(unsigned short i = 1; i < argc; i++){
+        std::string curr = argv[i];
+        if (curr == "--ip" && (i+1)<argc) { 
+            target = argv[i+1]; 
+            i++;
+        } else if (curr[0] == '-') // flag
+        {
+            flag_parser(&i, argv);
+        }
+        
+    }
 }
-#include "functions.hpp"
 
 void flag_parser(unsigned short *index, char *argv[])
 {
-    (void)index;
-    (void)argv;
+    std::string curr = argv[*index];
+
+    if (curr == "-t") {
+        std::cout << "test" << std::endl;
+    } 
 }
